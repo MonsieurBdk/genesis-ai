91bfa67bea933361a1ed249c297f932160a92d18
Don De Dieu Bundjoko <99693799+MonsieurBdk@users.noreply.github.com>
2024-11-20 16:12:19 +0100

    Update settings.py

--- src/ui/settings.py
+++ src/ui/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'ui.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [[BASE_DIR/"templates"]],
+        'DIRS': [BASE_DIR/"templates"],
         
         'APP_DIRS': True,
         'OPTIONS': {
